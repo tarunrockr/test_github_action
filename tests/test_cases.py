301076c126f3f8ab61887155918567dd85c760f0
tarunrockr <tarun94060sharma@gmail.com>
2024-09-07 17:07:37 +0530

    Updated test case file

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,11 +1,11 @@
 from src.app import add, sub
 
 
-def test_add(a,b):
+def test_add():
     assert add(2,3)  == 5
     assert add(-3,1) == -2
 
-def test_sub(a,b):
+def test_sub():
     assert sub(5,3)   == 2
     assert sub(-5,-3) == -2
     assert sub(-5,3)  == -8
