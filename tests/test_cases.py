476c698d3b6e1d91156b05a97a72e105b3fa9c3d
tarunrockr <tarun94060sharma@gmail.com>
2024-09-07 13:31:39 +0530

    test case file

---
+++ tests/test_cases.py
@@ -0,0 +1,11 @@
+from src.app import add, sub
+
+
+def test_add(a,b):
+    assert add(2,3)  == 5
+    assert add(-3,1) == -2
+
+def test_sub(a,b):
+    assert sub(5,3)   == 2
+    assert sub(-5,-3) == -2
+    assert sub(-5,3)  == -8
